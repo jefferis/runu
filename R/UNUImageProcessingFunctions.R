91a6a60d47872402fbfbc102f926afbe99fe358f
Gregory Jefferis <jefferis@gmail.com>
2017-07-25 20:28:55 +0100

    Add full roxygen documentation

--- R/UNUImageProcessingFunctions.R
+++ R/UNUImageProcessingFunctions.R
@@ -2,15 +2,29 @@
 # unu = Utah Nrrd Utilities - ie they operate on nrrd files
 # See http://teem.sourceforge.net/unrrdu/index.html
 
+
+#' Binary operation on two nrrds, or on a nrrd and a constant
+#'
+#' @param infiles Input files
+#' @param outfile Output files
+#' @param fun Binary function or operator
+#' @param gzip Whether to gzip the resultant file
+#' @param CreateDirs Whether to create implied output directories
+#' @param Verbose Print status messages
+#' @param Force Whether to overwrite an existing file
+#'
+#' @return The number of input files
+#' @export
+#' @importFrom nat.utils RunCmdForNewerInput
 Nrrd2op<-function(infiles,outfile,fun=c("max","min","+", "-", "x", "/"),
 	gzip=FALSE,CreateDirs=TRUE,Verbose=TRUE,Force=FALSE){
 	if(length(infiles)<1) return(NULL)
 	# Do nothing if inputs are older than output unless Force=T
 	if(!Force && !RunCmdForNewerInput(NULL,infiles,outfile)) return (NULL)
 	if(CreateDirs && !file.exists(dirname(outfile))) dir.create(dirname(outfile))
-	fun=match.arg(fun)
-	
-	# 1 input: f1 -> output
+  fun=match.arg(fun)
+
+  # 1 input: f1 -> output
 	if(length(infiles)==1) file.copy(infiles[1],outfile)
 	else if(length(infiles)>1) {
 		# 2 or more inputs: f1 x f2 -> output
@@ -35,14 +49,14 @@ Nrrd2op<-function(infiles,outfile,fun=c("max","min","+", "-", "x", "/"),
 
 #' Return range of values in nrrd file (interface to unu minmax command)
 #' @param filename nrrd file containing data
-#' @param Blind8 Assume 8 bit data has range 0-255 (or -127-127), default F.
-#' @param ... passed to \link{\code{system}} function
-#' @return c(min,max) or c(NA,NA) like R's \link{\code{range}} function
+#' @param blind8 Assume 8 bit data has range 0-255 (or -127-127), default F.
+#' @param ... passed to \code{\link{system}} function
+#' @return c(min,max) or c(NA,NA) like R's \code{\link{range}} function
 #' @author jefferis
 #' @export
-NrrdMinMax<-function(filename, Blind8=FALSE, ...){
+NrrdMinMax<-function(filename, blind8=FALSE, ...){
 	args=shQuote(filename)
-	if(Blind8) args=c(args,"-blind8 true")
+	if(blind8) args=c(args,"-blind8 true")
 	minmax=.callunu("minmax",args,intern=TRUE,...)
 	if(length(minmax)==0) return(c(NA,NA))
 	as.numeric(sub("(min|max): ","",minmax[1:2]))
@@ -50,38 +64,43 @@ NrrdMinMax<-function(filename, Blind8=FALSE, ...){
 
 #' Resample a nrrd with simple or complex filters
 #'
-#' wraps unu resample
-#' 
-#' When size is explicitly an integer (e.g. c(512L,512L,100L) then it is 
-#' assumed to represent the size of the desired output in pixels.
-#' 
-#' unu resample defaults to cell centering in the asbence of information, but I 
-#' prefer node. 
-#' 
-#' * downsampling a **cell** image (e.g. x2) will result in 
-#'   * an origin shift **iff** an origin was specified in the input file
-#'   * a change in the space directions that is not an exact multiple of the downsampling factor 
-#' * downsampling a node image (e.g. x2) will result in:
-#'   * no change in origin
-#'   * a doubling in the space directions field
-#' @param infile 
-#' @param outfile 
-#' @param size Numeric vector of scale factors (NA=> don't touch) or 
-#'   integer vector of pixel dimensions. 
+#' @description wraps unu resample
+#'
+#'   When size is explicitly an integer (e.g. c(512L,512L,100L) then it is
+#'   assumed to represent the size of the desired output in pixels.
+#'
+#'   unu resample defaults to cell centering in the asbence of information, but
+#'   I prefer node.
+#'
+#'   * downsampling a **cell** image (e.g. x2) will result in \itemize{
+#'
+#'   \item an origin shift **iff** an origin was specified in the input file
+#'
+#'   \item a change in the space directions that is not an exact multiple of the
+#'   downsampling factor } Downsampling a node image (e.g. x2) will result in:
+#'   \itemize{
+#'
+#'   \item no change in origin
+#'
+#'   \item a doubling in the space directions field }
+#' @param infile,outfile Input/output files
+#' @param size Numeric vector of scale factors (NA=> don't touch) or integer
+#'   vector of pixel dimensions.
 #' @param voxdims Target voxel dimensions
-#' @param centering Whether to assume node or cell centering if not specified
-#'   in input file.
+#' @param centering Whether to assume node or cell centering if not specified in
+#'   input file.
 #' @param otherargs Passed to unu resample
-#' @param gzip 
-#' @param suffix 
-#' @param CreateDirs 
-#' @param Verbose 
-#' @param Force 
-#' @param UseLock 
+#' @param gzip Whether or not to gzip encode the output file?
+#' @param suffix A suffix added to the input file to construct an output
+#'   filename
+#' @param UseLock Whether to use file-based locking to enable simple
+#'   parallelisation
 #' @param ... Additional params passed to .callunu
-#' @return 
+#' @return The output file
 #' @author jefferis
 #' @export
+#' @importFrom nat.utils makelock
+#' @inheritParams Nrrd2op
 NrrdResample<-function(infile,outfile,size,voxdims=NULL,
 		centering=c("cell","node"),otherargs=NULL,gzip=TRUE, suffix=NULL,
 		CreateDirs=TRUE,Verbose=TRUE,Force=FALSE,UseLock=FALSE,...){
@@ -143,9 +162,27 @@ NrrdResample<-function(infile,outfile,size,voxdims=NULL,
 	else system(fullcmd,...)
 }
 
+#' Title
+#'
+#' @details \code{bins} defaults to 1000 for float/double image data, to the
+#'   number of levels between \code{min} and \code{max} for integer data. If
+#'   \code{min} or \code{max} is not specified, they will be set to the minimum
+#'   (maximum) value found in the input data.
+#' @param maskfile Path to another image file defining which pixels in the input
+#'   file to include.
+#' @param bins Number of bins for the histogram (see details)
+#' @param min,max Minimum and maximum values for the histogram (see details)
+#' @param blind8 Whether to assume the full range (0,255) for 8 bit images
+#'   without checking
+#' @param ... Additional arguments passed to private \code{.callunu} function.
+#'
+#' @return Output file
+#' @inheritParams NrrdResample
+#' @importFrom nat read.nrrd.header
+#' @export
 NrrdHisto<-function(infile,outfile=sub("\\.([^.]+)$",".histo.\\1",infile),
 	maskfile,bins,min,max,blind8=TRUE,...){
-	h=nat::read.nrrd.header(infile)
+	h=read.nrrd.header(infile)
 	nrrdType=nat:::.standardNrrdType(h$type)
 	unuhistooptions=''
 	if(nrrdType%in%c("uint8","int8") && blind8 && missing(min) && missing(max)){
@@ -203,6 +240,15 @@ NrrdQuantize<-function(infile,outfile,min,max,bits=c("8","16","32"),
 	return(TRUE)
 }
 
+#' Test integrity of a NRRD file using gzip/bzip2 checksum
+#'
+#' @param defaultReturnVal The value to return when file cannot be checked
+#'
+#' @return A logical indicating if the check passed
+#' @export
+#'
+#' @inheritParams NrrdResample
+#' @seealso \code{\link{NrrdTestDataLength}} which is much faster
 NrrdTestIntegrity<-function(infile,defaultReturnVal=TRUE){
 	# Tests integrity of a compressed nrrd file using crc check
 	if(!file.exists(infile)) return(NA)
@@ -217,12 +263,19 @@ NrrdTestIntegrity<-function(infile,defaultReturnVal=TRUE){
 	return(testval==0)
 }
 
+#' Quick test that gzipped NRRD has correct data length
+#'
+#' @details Tests integrity of a nrrd file by checking that the data block is as
+#'   long as it should be. For a gzip file, it checks that the last 4 bytes
+#'   encode the length of the uncompressed data. This says nothing about the
+#'   contents but does ensure that the file has not been truncated which is much
+#'   the most common problem.
+#' @return A logical indicating if the check passed
+#' @export
+#' @inheritParams NrrdTestIntegrity
+#' @seealso \code{\link{NrrdTestIntegrity}} which is more comprehensive but much
+#'   slower
 NrrdTestDataLength<-function(infile,defaultReturnVal=TRUE){
-	# Tests integrity of a nrrd file by checking that the data block is as long
-	# as it should be. For a gzip file, it checks that the last 4 bytes
-	# encode the length of the uncompressed data. This says nothing about the contents
-	# but does ensure that the file has not been truncated which is much the most
-	# common problem.
 	if(!file.exists(infile)) return(NA)
 	# need to read this full header in order to get the size in bytes of
 	# the uncompressed data
@@ -263,14 +316,19 @@ NrrdTestDataLength<-function(infile,defaultReturnVal=TRUE){
 	}
 }
 
-NrrdCrc<-function(infile,UseGzip=FALSE,FastHeader=TRUE){
-	# gets the CRC (hash) of a gzip encoded nrrd
-	# Defaults to a quick method based on
-	# knowledge of gzip file format from:
-	# http://www.gzip.org/zlib/rfc-gzip.html
-	# and assumption that there is only one member in gzip data
-	# can also use gzip but this is much slower since have to copy
-	# unu data to temporary file
+#' Find the CRC (hash) of a gzip encoded NRRD file
+#'
+#' @param UseGzip Whether to use the gzip command line tool to extract CRC
+#'   (default \code{FALSE}, see details)
+#' @param FastHeader Parse only essential parts of NRRD header to save time.
+#'
+#' @details: Defaults to a quick method based on knowledge of gzip file format
+#' from: \url{http://www.gzip.org/zlib/rfc-gzip.html} and assumption that there
+#' is only one member in gzip data. Can also use gzip but this is much slower
+#' since have to copy unu data to temporary file.
+#' @inheritParams NrrdResample
+NrrdCrc<-function(infile, UseGzip=FALSE, FastHeader=TRUE){
+
 	if(!file.exists(infile)) return(NA)
 
 	ext=tolower(sub(".*\\.([^.]+)$","\\1",basename(infile)))
@@ -333,25 +391,94 @@ NrrdCrc<-function(infile,UseGzip=FALSE,FastHeader=TRUE){
 }
 
 
-#' Make projections along a nrrd axis
+#' Make projections along a nrrd axis, typically also rescaling
 #'
-#' @details gamma: Just as in xv, the gamma value here is actually the reciprocal
-#'   of the exponent actually used to transform the values.
-#' Note also that for \code{cropmin,cropmax} the special value M can be used
-#'   to indicate the maximum index for that axis (i.e. n-1 when there are n
-#'   samples).
-#' @param infile Path to input file
-#' @param outfile Optional path to output file (constructed automatically when missing)
+#' @details gamma: Just as in xv, the gamma value here is actually the
+#'   reciprocal of the exponent actually used to transform the values. Note also
+#'   that for \code{cropmin,cropmax} the special value M can be used to indicate
+#'   the maximum index for that axis (i.e. n-1 when there are n samples).
+#'
+#'   scale: For each axis, information about how many samples in output:
+#'   \itemize{
+#'
+#'   \item "=": leave this axis completely untouched: no resampling whatsoever
+#'   \item "x<float>": multiply the number of input samples by <float>, and
+#'   round to the nearest integer, to get the number of output samples. Use "x1"
+#'   to resample the axis but leave the number of samples unchanged
+#'
+#'   \item "/<float>": divide number of samples by <float>
+#'
+#'   \item "+=<uint>", "-=<uint>": add <uint> to or subtract <uint> from number
+#'   input samples to get number output samples
+#'
+#'   \item "<uint>": exact number of output samples
+#'
+#'   \item "a": resample this axis to whatever number of samples preserves the
+#'   aspect ratio of other resampled axes. Currently needs to be used on all but
+#'   one of the resampled axes, if at all. (1 or more sampling specifications) }
+#'
+#'   Kernel: The kernel to use for resampling. Kernels logically live in the
+#'   input index space for upsampling, and in the output index space for
+#'   downsampling. Possibilities include: \itemize{ \item "box": nearest
+#'   neighbor interpolation on upsampling, and uniform averaging on downsampling
+#'   \item "cheap": nearest neighbor interpolation for upsampling, and
+#'   non-blurring sub-sampling (pick subset of input samples) on downsampling
+#'
+#'   \item "tent": linear interpolation
+#'
+#'   \item "cubic:B,C": Mitchell/Netravali BC-family of cubics: "cubic:1,0":
+#'   B-spline; maximal blurring "cubic:0,0.5": Catmull-Rom; good interpolating
+#'   kernel
+#'
+#'   \item "c4h": 6-sample-support, C^4 continuous, accurate
+#'
+#'   \item "c4hai": discrete pre-filter to make c4h interpolate
+#'
+#'   \item "bspl3", "bspl5", "bspl7": cubic (same as cubic:1,0), quintic, and
+#'   7th order B-spline
+#'
+#'   \item "bspl3ai", "bspl5ai", "bspl7ai": discrete pre-filters to make bspl3,
+#'   bspl5, bspl7 interpolate
+#'
+#'   \item "hann:R": Hann (cosine bell) windowed sinc, radius R
+#'
+#'   \item "black:R": Blackman windowed sinc, radius R
+#'
+#'   \item "gauss:S,C": Gaussian blurring, with standard deviation S and cut-off
+#'   at C standard deviations
+#'
+#'   \item "dgauss:S,C": Lindeberg's discrete Gaussian. default: "cubic:0,0.5" }
+#'
+#'   Crop max, min: \itemize{
+#'
+#'   \item <int> gives 0-based index
+#'
+#'   \item M, M+<int>, M-<int> give index relative to the last sample on the
+#'   axis (M == #samples-1).
+#'
+#'   \item m+<int> give index relative to minimum axis (only relevant for max)
+#'
+#'   }
+#' @param scale How to scale each axis (see details)
+#' @param kernel Type of smoothing kernel to use (see details)
+#' @param gamma Exponent for the mapping of input to output values (see
+#'   details)
+#' @param outfile Optional path to output file (constructed automatically when
+#'   missing)
 #' @param axis Number indicating 0-indexed axis or character "x", "y" or "z"
 #' @param measure Character vector indicating summary function to apply to
 #'   values in each column. Choose from \code{c("max", "min", "mean", "median",
 #'   "mode", "variance", "skew", "intc", "slope", "error", "sd", "product",
 #'   "sum", "L1", "L2", "Linf")}
+#' @param cropmin,cropmax triples defining the lower and higher corners of
+#'   bounding box
 #' @return Logical indicating success
 #' @export
-#' @seealso \code{\link{NrrdSave},\link{NrrdMerge}}
+#' @seealso \code{\link{NrrdSave}}, \code{\link{NrrdMerge}}
+#' @inheritParams NrrdResample
 #' @examples
 #' \dontrun{
+#' # NB cropping from 50th slice to 50 before final slice
 #' NrrdProject(infile,axis='z',cropmin='0 0 50',cropmax='0 0 M-50')
 #' }
 NrrdProject<-function(infile,outfile,axis,
@@ -398,6 +525,16 @@ NrrdProject<-function(infile,outfile,axis,
 	return(TRUE)
 }
 
+#' Merge multiple NRRD images into a multichannel PNG
+#'
+#' @param outdir Output directory
+#' @param axis 0-indexed axis to join (merge) along (defaults to 0)
+#' @param ... Currently ignored
+#'
+#' @inheritParams Nrrd2op
+#' @inheritParams NrrdResample
+#' @return Logical value indicating whether output exists
+#' @export
 NrrdMerge<-function(infiles,outdir=NULL,outfile=NULL,axis=0,
 	CreateDirs=TRUE,Verbose=TRUE,Force=FALSE,UseLock=FALSE,...){
 	if(is.null(outfile) && is.null(outdir)) {
@@ -410,16 +547,20 @@ NrrdMerge<-function(infiles,outdir=NULL,outfile=NULL,axis=0,
 		outfile=sub("\\.[^.]+$",".png",outfile)
 	}
 
-	if(!all(file.exists(infiles))) stop("one or more infiles: ",infiles," do not exist")
+	if(!all(file.exists(infiles)))
+	  stop("one or more infiles: ",infiles," do not exist")
 	# return TRUE to signal output exists (we just didn't make it now)
 	if(!Force && !RunCmdForNewerInput(NULL,infiles,outfile)) return (TRUE)
-	if(CreateDirs && !file.exists(dirname(outfile))) dir.create(dirname(outfile),recursive = TRUE)
+	if(CreateDirs && !file.exists(dirname(outfile)))
+	  dir.create(dirname(outfile),recursive = TRUE)
 	lockfile=paste(outfile,sep=".","lock")
 	# return FALSE to signal output doens't exist
 	if(UseLock && !makelock(lockfile)) return (FALSE)
 
-	cmd=paste("unu join -a",axis,"-incr -i",paste(shQuote(infiles),collapse=" "),"-o",shQuote(outfile))
-	cat(cmd,"\n")
+	cmd=paste("unu join -a", axis, "-incr -i",
+	          paste(shQuote(infiles),collapse=" "),
+	          "-o",shQuote(outfile))
+	cat(cmd, "\n")
 	rval = system(cmd)
 	if(Verbose) cat(".")
 	if(UseLock) unlink(lockfile)
@@ -427,6 +568,19 @@ NrrdMerge<-function(infiles,outdir=NULL,outfile=NULL,axis=0,
 	return(TRUE)
 }
 
+#' Flip a NRRD image along one axis
+#'
+#' @param axes Which axes to flip (0-indexed integer)
+#' @param suffix Suffix to add to input file to construct output file.
+#' @param endian Whether output image should be big (default) or little endian
+#'   for multibyte data types (essentially all other than 8 bit)
+#' @param OverWrite Whether to overwrite (yes or no) or update output image.
+#'
+#' @return \code{TRUE} if output exists
+#' @export
+#'
+#' @inheritParams NrrdResample
+#' @inheritParams NrrdProject
 NrrdFlip<-function(infile,outfile,axes,suffix=NULL,endian=c("big","little"),
 	CreateDirs=TRUE,Verbose=TRUE,UseLock=FALSE, OverWrite=c("no","update","yes")){
 	# TODO would be nice if we could
@@ -479,7 +633,7 @@ NrrdFlip<-function(infile,outfile,axes,suffix=NULL,endian=c("big","little"),
 #' Save a nrrd file in a different encoding / format
 #'
 #' In outfile is missing, will overwrite infile
-#' @param infile, outfile Paths to input and output nrrds
+#' @param infile,outfile Paths to input and output nrrds
 #' @param format (one of 6 supported by unu save, default nrrd)
 #' @param encoding (one of 5 supported by unu save, default gz when nrrd)
 #' @param CreateDirs Whether to make missing direcories implied by output path
