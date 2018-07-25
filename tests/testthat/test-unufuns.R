8308d856e64758d6f802d60bd714cb8a0b3a8632
Gregory Jefferis <jefferis@gmail.com>
2018-07-25 09:36:36 +0100

    proper test for unu hist

--- tests/testthat/test-unufuns.R
+++ tests/testthat/test-unufuns.R
@@ -1,7 +1,20 @@
 context("test-unufuns.R")
+library(nat)
+
+testim=im3d(1:24, dims=c(2,3,4))
+write.nrrd(testim, testimf <- tempfile(fileext = '.nrrd'), dtype = 'byte')
+on.exit(unlink(testimf))
 
 test_that("we can get unu information", {
   skip_no_unu()
   expect_match(paste(unu('env', intern=TRUE), collapse = ""),
                'environment variables')
+})
+
+test_that("unu hist", {
+  imh=hist(testim, breaks = 0:256-0.5)
+  nh=NrrdHisto(testimf, outfile = F)
+  # don't test setting the name
+  imh$xname=nh$xname
+  expect_equal(imh, nh)
 })
